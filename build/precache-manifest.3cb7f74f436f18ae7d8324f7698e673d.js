7e47aee0842bd93f1cbe012664b9255bbf22e02f
rowgregory <rowgregory@gmail.com>
2023-01-20 10:57:41 -0500

    Configure react app for deployment to GitHub Pages

---
+++ build/precache-manifest.3cb7f74f436f18ae7d8324f7698e673d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412195ce5127ba77fba5c7bd94a9ba27",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "c699281b0eb9fa890e48",
+    "url": "/hangman/static/css/main.6c74f4cd.chunk.css"
+  },
+  {
+    "revision": "aa6a102fdf4d126a4460",
+    "url": "/hangman/static/js/2.64fa1b6c.chunk.js"
+  },
+  {
+    "revision": "16dc667838228601f161e0fab57afa83",
+    "url": "/hangman/static/js/2.64fa1b6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c699281b0eb9fa890e48",
+    "url": "/hangman/static/js/main.940b49c5.chunk.js"
+  },
+  {
+    "revision": "82f934fb7475cbae144f",
+    "url": "/hangman/static/js/runtime-main.080680ab.js"
+  }
+]);
